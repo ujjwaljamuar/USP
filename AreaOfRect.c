33de1f56b0454c4c8ad878bfad4f0270fdd4ae2e
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2021-11-02 12:03:15 +0530

    Update AreaOfRect.c

--- AreaOfRect.c
+++ AreaOfRect.c
@@ -4,9 +4,14 @@ double area(double l, double b)
     double area = l * b;
     return area;
 }
+void print(){
+	double l, b;
+    	printf("Enter length and breadth : \n");
+    	scanf("%lf %lf", &l, &b);
+	printf("Area = %lf sq. unit.", area(l, b));
+	}
 int main()
 {
-    double l, b;
-    printf("Enter length and breadth : \n");
-    scanf("%lf %lf", &l, &b);
-    printf("Area = %lf sq. unit.", area(l, b));
+    print();
+    return 0;
+    }
